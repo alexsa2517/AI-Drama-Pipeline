ed9e003abca30dbf86780014925da37643db174c
alexsa2517 <admoney4@gmail.com>
2026-09-07 15:23:33 +0700

    feat: integrate hard scene visual lock into scene packs

--- src/prompt_pack.py
+++ src/prompt_pack.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from .continuity import build_character_context, scene_continuity_context
+from .continuity import build_character_context, build_scene_visual_lock, scene_continuity_context
 from .prompt_builder import build_image_prompt, build_video_prompt, build_voice_prompt
 from .animation_rules import build_character_animation_context
 from .dialogue_scene import build_camera_plan, build_conversation_prompt
@@ -14,6 +14,7 @@ def build_scene_pack(episode: dict) -> list[dict]:
     character_context = build_character_context(episode)
     packs = []
     for index, scene in enumerate(episode.get("scenes", [])):
+        visual_lock = build_scene_visual_lock(scene)
         conversation = build_conversation_prompt(scene)
         camera_plan = build_camera_plan(scene)
         shot_director = build_shot_director_prompt(scene)
@@ -21,7 +22,7 @@ def build_scene_pack(episode: dict) -> list[dict]:
         timeline_prompt = build_timeline_prompt(scene)
         audio_direction = build_audio_direction(scene)
         dialogue_video = build_dialogue_video_prompt(episode, scene)
-        video_parts = [build_video_prompt(episode, scene)]
+        video_parts = [visual_lock, build_video_prompt(episode, scene)]
         if dialogue_video:
             video_parts.append(dialogue_video)
         if conversation:
@@ -34,7 +35,8 @@ def build_scene_pack(episode: dict) -> list[dict]:
         video_parts.append(scene_continuity_context(episode, index))
         packs.append({
             "scene_id": scene["id"],
-            "image": build_image_prompt(episode, scene) + "\n\n" + character_context,
+            "visual_lock": visual_lock,
+            "image": visual_lock + "\n\n" + build_image_prompt(episode, scene) + "\n\n" + character_context,
             "video": "\n\n".join(video_parts),
             "voice": build_voice_prompt(episode, scene),
             "dialogue": conversation,
