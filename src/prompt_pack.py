6dfc39178ab1f0a5982d15e3b6a7e29c61be2af3
alexsa2517 <admoney4@gmail.com>
2026-09-13 09:36:23 +0700

    feat: inject professional acting direction into scene prompts

--- src/prompt_pack.py
+++ src/prompt_pack.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .continuity import build_character_context, build_scene_visual_lock, scene_continuity_context
 from .prompt_builder import build_image_prompt, build_video_prompt, build_voice_prompt
 from .animation_rules import build_character_animation_context
+from .ai_acting_director import build_acting_prompt
 from .dialogue_scene import build_camera_plan, build_conversation_prompt
 from .speech_pipeline import build_dialogue_video_prompt
 from .shot_director import build_shot_director_prompt
@@ -22,6 +23,7 @@ def build_scene_pack(episode: dict) -> list[dict]:
         timeline_prompt = build_timeline_prompt(scene)
         audio_direction = build_audio_direction(scene)
         dialogue_video = build_dialogue_video_prompt(episode, scene)
+        acting_director = build_acting_prompt(scene)
         video_parts = [visual_lock, build_video_prompt(episode, scene)]
         if dialogue_video:
             video_parts.append(dialogue_video)
@@ -31,6 +33,7 @@ def build_scene_pack(episode: dict) -> list[dict]:
             video_parts.append(shot_director)
             video_parts.append(timeline_prompt)
         video_parts.append(audio_direction)
+        video_parts.append(acting_director)
         video_parts.append(build_character_animation_context(episode, scene))
         video_parts.append(scene_continuity_context(episode, index))
         packs.append({
@@ -40,6 +43,7 @@ def build_scene_pack(episode: dict) -> list[dict]:
             "video": "\n\n".join(video_parts),
             "voice": build_voice_prompt(episode, scene),
             "dialogue": conversation,
+            "acting_director": acting_director,
             "camera_plan": camera_plan,
             "shot_director": shot_director,
             "dialogue_timeline": timeline,
